eaa15a5d14ddd8fdafa4152f42ae4cd515d6fbb1
Ryan Campbell <ryan@biznas.io>
2017-04-21 11:32:13 -0700

    static fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MdButtonModule,
-  MdCheckboxModule,
-  MdInputModule,
-  MdListModule,
-  MdSelectModule,
-  MdToolbarModule,
-} from '@angular/material';
+
 import { Framing } from '@framing/ng-core';
 
 import { AppComponent } from './app.component';
@@ -18,32 +12,29 @@ import { NavComponent } from './nav/nav.component';
 import { DataService } from './data.service';
 import { RebelsModule } from './rebels/rebels.module';
 
-export const ngModuleConfig = Framing((framing) => framing
-  .root()
-  .children([
-    { path: '', pathMatch: 'full', redirectTo: 'rebels', },
-    { path: 'rebels', loadChildren: () => RebelsModule },
-    { path: 'planets', loadChildren: 'app/planets/planets.module#PlanetsModule' },
-    { path: '**', pathMatch: 'full', redirectTo: 'rebels' },
-  ])
-  .declarations([
-    NavComponent,
-  ])
-  .imports([
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
-    MdButtonModule,
-    MdCheckboxModule,
-    MdInputModule,
-    MdListModule,
-    MdSelectModule,
-    MdToolbarModule,
-  ])
-  .provider(DataService)
-  .componentAndDeclare(AppComponent),
-);
+export function config(framing) {
+  return framing
+    .root()
+    .children([
+      { path: '', pathMatch: 'full', redirectTo: 'rebels', },
+      { path: 'rebels', loadChildren: () => RebelsModule },
+      { path: 'planets', loadChildren: 'app/planets/planets.module#PlanetsModule' },
+      { path: '**', pathMatch: 'full', redirectTo: 'rebels' },
+    ])
+    .declarations([
+      NavComponent,
+    ])
+    .imports([
+      BrowserAnimationsModule,
+      FormsModule,
+      HttpModule,
+      MaterialModule,
+    ])
+    .provider(DataService)
+    .componentAndDeclare(AppComponent);
+}
+
+export const ngModuleConfig = Framing(config);
 
 @NgModule(ngModuleConfig)
 export class AppModule { }
