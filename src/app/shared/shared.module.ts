9418d50a9869950d30f9420ef5d7432f16074ee9
John Papa <john@Johns-MBP.lan>
2017-05-09 01:11:25 -0400

    dead code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MdButtonModule,
   MdCheckboxModule,
