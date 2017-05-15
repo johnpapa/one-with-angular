87cd6ddb039aaecea14b2a172dfa8de43c40fa22
John Papa <john@Johns-MBP.lan>
2017-05-14 21:25:53 -0400

    format

--- src/app/planets/planet-list/planet-list.component.ts
+++ src/app/planets/planet-list/planet-list.component.ts
@@ -23,13 +23,12 @@ export class PlanetListComponent implements OnInit {
 
   ngOnInit() {
     this.dataService.getPlanets()
-      .subscribe(
-      planets => {
+      .subscribe(planets => {
         this.planets = planets;
         this.snackBar.open('Getting Planets data succeeded', 'HTTP', this.configService.snackConfig);
       },
       () => this.snackBar.open('Getting Planets data failed', 'ERROR', this.configService.snackConfig)
-    );
+      );
   }
 
   selectPlanet(planet: Planet) {
