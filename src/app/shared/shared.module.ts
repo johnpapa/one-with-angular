b66bb57cea8635d8ff8eec32dee20da4a573e55a
John Papa <john@Johns-MBP.lan>
2017-05-14 21:37:41 -0400

    removed http from shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import {
   MdButtonModule,
   MdCheckboxModule,
@@ -19,7 +18,6 @@ import { PieChartModule } from '@swimlane/ngx-charts';
 const modules = [
   CommonModule,
   FormsModule,
-  HttpModule,
   MdButtonModule,
   MdCheckboxModule,
   MdInputModule,
