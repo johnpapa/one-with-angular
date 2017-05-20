8b3d3d8708bd5c508626294764f2b40234409472
John Papa <john@Johns-MBP.lan>
2017-05-19 20:26:33 -0400

    adding fallback and express server

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,18 @@
-var express = require('express'),
-  path = require('path'),
-  fs = require('fs');
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
 
-var app = express();
-var staticRoot = __dirname + '/';
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname))
 
-app.set('port', (process.env.PORT || 8080));
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
 
-app.use(express.static(staticRoot));
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
 
-app.use(function (req, res, next) {
-
-  // if the request is not html then move along
-  var accept = req.accepts('html', 'json', 'xml');
-  if (accept !== 'html') {
-    return next();
-  }
-
-  // if the request has a '.' assume that it's for a file, move along
-  var ext = path.extname(req.path);
-  if (ext !== '') {
-    return next();
-  }
-
-  fs.createReadStream(staticRoot + 'index.html').pipe(res);
-
-});
-
-app.listen(app.get('port'), function () {
-  console.log('app running on port', app.get('port'));
-});
+app.listen(8080, function () {
+  console.log( 'Express serving on 8080!' )
+})
