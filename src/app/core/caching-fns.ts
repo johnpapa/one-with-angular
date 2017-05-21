fc3ee488a4fee581d7f31d6b49f67adf45da0cf9
John Papa <john@Johns-MBP.lan>
2017-05-20 21:26:15 -0400

    updated caching fns

--- src/app/core/caching-fns.ts
+++ src/app/core/caching-fns.ts
@@ -1,10 +1,14 @@
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Subject } from 'rxjs/Subject';
+
 import 'rxjs/add/observable/timer';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/switchMap';
 
 /* Whether to log caching activity to console. For debugging/demos. */
@@ -21,10 +25,16 @@ export type Notifier = (msg: NotificationMessage) => void;
 /**
  * Cache values and update from source whenever next emits
  * @param {Observable<T>} source The async source of the data
- * @param {Observable<any>} update When this observable emits, update the cache from the source
+ * @param {Observable<any>} updateWhen When this observable emits, update the cache from the source
+ * @param {T | ReplaySubject<T> | BehaviorSubject<T>} [initialValue] Optional.
+ *   Initial value of cache before first source execution or the Subject<T> to use.
  */
-export function createCache<T>(source: Observable<T>, updateWhen: Observable<any>) {
-  const cacheSubject = new ReplaySubject(1);
+export function createCache<T>(
+  source: Observable<T>,
+  updateWhen: Observable<any>,
+  initialValue?: T | ReplaySubject<T> | BehaviorSubject<T>): Observable<T> {
+
+  const cacheSubject = getSubject<T>(initialValue);
 
   // get from the source whenever `next` emits
   updateWhen
@@ -33,28 +43,6 @@ export function createCache<T>(source: Observable<T>, updateWhen: Observable<any
 
   return cacheSubject.asObservable();
 };
-
-/*
- * Invoke async observable source (e.g., HTTP call), cache the result
- * and then refresh the cached value from a new HTTP call periodically
- * with a timer that ticks every `expirationPeriod` ms.
- *
- * Example:
- *
- *   const cachingRequest = this.http.get('hero.json')
- *     .map(res => res.json().data)
- *     .let(timerCache());
- *
- *   cachingRequest.subscribe(data => ... do something ...);
- */
-export function createTimerCache<T>(expirationPeriod = defaultExpirationPeriod) {
-  return (source: Observable<T>) =>
-    createCache(
-      source,
-      Observable.timer(0, expirationPeriod)
-        .do(count => log('source executed #' + count))
-    );
-}
 
 /*
  * Cache values from an async source (e.g., HTTP call) in an observable.
@@ -88,15 +76,18 @@ export function createTimerCache<T>(expirationPeriod = defaultExpirationPeriod) 
  *   update();
  *
  * @param {Observable<T>} source Async source of values.
- * @param {Observable<boolean>} update The "demand" observable that may update the cache.
+ * @param {Observable<boolean>} updateWhen The "demand" observable that may update the cache.
  * @param {Notifier} [notifier] Optional. Called with caching activity messages.
  * @param {number} [expirationPeriod=defaultExpirationPeriod] Expiration window.
+ * @param {T | ReplaySubject<T> | BehaviorSubject<T>} [initialValue] Optional.
+ *   Initial value of cache before first source execution or the Subject<T> to use.
  */
 export function createOnDemandCache<T>(
   source: Observable<T>,
   updateWhen: Observable<boolean>,
   notifier?: Notifier,
-  expirationPeriod?: number
+  expirationPeriod?: number,
+  initialValue?: T | ReplaySubject<T> | BehaviorSubject<T>
 ): Observable<T> {
   let expired = 0;
   let firstTime = true;
@@ -119,12 +110,56 @@ export function createOnDemandCache<T>(
         firstTime = false;
         notifier(fetch ?
           new NotificationMessage('fetching', 'Fetching...') :
-          new NotificationMessage('cached', 'Use cached value.' )
+          new NotificationMessage('cached', 'Use cached value.')
         );
         return fetch;
-      })
+      }),
+
+    initialValue
   );
 };
+
+/*
+ * Invoke async observable source (e.g., HTTP call), cache the result
+ * and then refresh the cached value from a new HTTP call periodically
+ * with a timer that ticks every `expirationPeriod` ms.
+ *
+ * Example:
+ *   const source = this.http.get('hero.json')
+ *     .map(res => res.json().data);
+ *
+ *   const timerCache = createTimerCache(source));
+ *
+ *   timerCache.subscribe(data => ... do something ...);
+ *
+ * @param {Observable<T>} source Async source of values.
+ * @param {number} [expirationPeriod=defaultExpirationPeriod] Cache refresh interval
+ * @param {T | ReplaySubject<T> | BehaviorSubject<T>} [initialValue] Optional.
+ *   Initial value of cache before first source execution or the Subject<T> to use.
+ */
+export function createTimerCache<T>(
+  source: Observable<T>,
+  expirationPeriod = defaultExpirationPeriod,
+  initialValue?: T | ReplaySubject<T> | BehaviorSubject<T>): Observable<T> {
+
+  const cacheSubject = getSubject<T>(initialValue);
+
+  return Observable.timer(0, expirationPeriod)
+    .switchMap(count => {
+      log('source executing #' + count);
+
+      // next executes source exactly once, then updates cache, then returns the cache
+      const next = source.first()
+        .do(data => log('Fetched data arrived'))
+        .do(data => cacheSubject.next(data))
+        .switchMap(() => cacheSubject);
+
+      // If first time, subscribe to next and return the cache immediately
+      // so subscribers can use it. Otherwise, return the next observable.
+      return count === 0 ? next.subscribe() && cacheSubject : next;
+    })
+    .share();
+}
 
 /** Message provided to notifiers. */
 export class NotificationMessage {
@@ -132,13 +167,17 @@ export class NotificationMessage {
     public type: 'cached' | 'fetching' | 'fetched' | 'error',
     public message?: string,
     public body?: any) {
-      body ? log(message, body) : log(message);
-    }
+    body ? log(message, body) : log(message);
+  }
 }
 
-function NullNotifier(msg: NotificationMessage) { }
+export function NullNotifier(msg: NotificationMessage) { }
 
 // logger logs to console when verbose == true
-function log(...args) {
+export function log(...args) {
   if (_verbose) { console.log.apply(null, args); }
+}
+
+function getSubject<T>(value?: T | BehaviorSubject<T> | ReplaySubject<T>) {
+  return value instanceof Subject ? value : new BehaviorSubject(value);
 }
