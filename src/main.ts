97aa60a48dd7ff9b9175167e1c4b2f17d148c8df
John Papa <john@Johns-MBP.lan>
2017-05-28 23:02:05 -0400

    service worker in main

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,52 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => registerServiceWorker());
+
+function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js').then(function (reg) {
+      reg.onupdatefound = function () {
+        const installingWorker = reg.installing;
+
+        installingWorker.onstatechange = function () {
+          switch (installingWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                console.log('New or updated content is available.');
+              } else {
+                console.log('Content is now available offline!');
+              }
+              break;
+
+            case 'redundant':
+              console.error('The installing service worker became redundant.');
+              break;
+          }
+        };
+      };
+    }).catch(function (e) {
+      console.error('Error during service worker registration:', e);
+    });
+  }
+}
+  // function registerServiceWorker() {
+
+  //   if ('serviceWorker' in navigator) {
+  //     window.addEventListener('load', load);
+  //   }
+
+  //   function load() {
+  //     navigator.serviceWorker.register('/service-worker.js').then(success, fail);
+  //   }
+
+  //   function success(registration) {
+  //     // Registration was successful
+  //     console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  //   }
+
+  //   function fail(err) {
+  //     // registration failed :(
+  //     console.log('ServiceWorker registration failed: ', err);
+  //   }
+  // }
